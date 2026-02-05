78385aa13c776b8c0736f203b407438bdf5141ec
Hrithik Reddy Chenreddy <122119750+hrxth-xk@users.noreply.github.com>
2026-02-05 15:08:08 +0530

    Add BookController with routes for home, books, authors, and categories

---
+++ W4/D1/S1/Q3.cs
@@ -0,0 +1,238 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Runtime.InteropServices;
+ 
+namespace dotnetapp.Controllers
+{
+    // [Route("[controller]")]
+    public class BookController : Controller
+    {
+        [Route("book/home")]
+ 
+        public IActoinResult Home(){
+            return View();
+        }
+        [Route("book/books")]
+ 
+        public IActoinResult Books(){
+            return View();
+        }
+        [Route("book/authors")]
+ 
+        public IActoinResult Authors(){
+            return View();
+        }
+        [Route("book/categories")]
+ 
+        public IActoinResult Categories(){
+            return View();
+        }
+    }
+}
+ 
+ 
+ 
+//Authors.cshtml
+ 
+ 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Runtime.InteropServices;
+ 
+namespace dotnetapp.Controllers
+{
+    // [Route("[controller]")]
+    public class BookController : Controller
+    {
+        [Route("book/home")]
+ 
+        public IActoinResult Home(){
+            return View();
+        }
+        [Route("book/books")]
+ 
+        public IActoinResult Books(){
+            return View();
+        }
+        [Route("book/authors")]
+ 
+        public IActoinResult Authors(){
+            return View();
+        }
+        [Route("book/categories")]
+ 
+        public IActoinResult Categories(){
+            return View();
+        }
+    }
+}
+ 
+ 
+ 
+ 
+//book.cshtml
+@page
+@model dotnetapp.Views.Book.Books
+@{
+    ViewData["Title"] = "Books";
+}
+ 
+<h1>@ViewData["Title"]</h1>
+ 
+<div>
+   
+</div>
+ 
+ 
+ 
+ 
+//Categories.cshtml
+@page
+@model dotnetapp.Views.Book.Categories
+@{
+    ViewData["Title"] = "Categories";
+}
+ 
+<h1>@ViewData["Title"]</h1>
+ 
+<div>
+   
+</div>
+
+//Bookcontroller
+ 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Runtime.InteropServices;
+ 
+namespace dotnetapp.Controllers
+{
+    // [Route("[controller]")]
+    public class BookController : Controller
+    {
+        [Route("book/home")]
+ 
+        public IActoinResult Home(){
+            return View();
+        }
+        [Route("book/books")]
+ 
+        public IActoinResult Books(){
+            return View();
+        }
+        [Route("book/authors")]
+ 
+        public IActoinResult Authors(){
+            return View();
+        }
+        [Route("book/categories")]
+ 
+        public IActoinResult Categories(){
+            return View();
+        }
+    }
+}
+ 
+ 
+ 
+//Authors.cshtml
+ 
+ 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Runtime.InteropServices;
+ 
+namespace dotnetapp.Controllers
+{
+    // [Route("[controller]")]
+    public class BookController : Controller
+    {
+        [Route("book/home")]
+ 
+        public IActoinResult Home(){
+            return View();
+        }
+        [Route("book/books")]
+ 
+        public IActoinResult Books(){
+            return View();
+        }
+        [Route("book/authors")]
+ 
+        public IActoinResult Authors(){
+            return View();
+        }
+        [Route("book/categories")]
+ 
+        public IActoinResult Categories(){
+            return View();
+        }
+    }
+}
+ 
+ 
+ 
+ 
+//book.cshtml
+@page
+@model dotnetapp.Views.Book.Books
+@{
+    ViewData["Title"] = "Books";
+}
+ 
+<h1>@ViewData["Title"]</h1>
+ 
+<div>
+   
+</div>
+ 
+ 
+ 
+ 
+//Categories.cshtml
+@page
+@model dotnetapp.Views.Book.Categories
+@{
+    ViewData["Title"] = "Categories";
+}
+ 
+<h1>@ViewData["Title"]</h1>
+ 
+<div>
+   
+</div>
+ 
+ 
+//home.cshtml
+@page
+@model dotnetapp.Views.Book.Home
+@{
+    ViewData["Title"] = "Home";
+}
+ 
+<h1>@ViewData["Title"]</h1>
+ 
+<div>
+   
+</div>
+ 
