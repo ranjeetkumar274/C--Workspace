e1ff8b6bda8df003d4863d0cdc4bc5dbce794a77
RANJEET KUMAR <147934359+ranjeetkumar274@users.noreply.github.com>
2026-02-13 16:19:00 +0530

    Create Q3.ts

---
+++ W6/D1/S2/Q3.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { PROMPTS } from 'src/prompts';
+
+@Component({
+  selector: 'app-promptdisplay',
+  templateUrl: './promptdisplay.component.html',
+  styleUrls: ['./promptdisplay.component.css']
+})
+export class PromptdisplayComponent implements OnInit {
+  prompts: string[] = PROMPTS;
+  currentPromptIndex: number = 0;
+  prompt: string;
+
+  constructor() {
+    this.nextPromt();
+  }
+
+  nextPromt() {
+    if (this.prompts.length > this.currentPromptIndex) {
+      this.prompt = this.prompts[this.currentPromptIndex];
+      this.currentPromptIndex++;
+    } else {
+      this.currentPromptIndex = 0;
+      this.prompt = this.prompts[this.currentPromptIndex];
+    }
+  }
+
+  ngOnInit(): void {}
+  
+  <div class="moal container">
+  <h1>Daily Prompts</h1>
+  
+  <p [innerText]="prompt"></p>
+  
+  <button (click)="nextPromt()">
+    Next
+  </button>
+</div>
+
+  
+}
